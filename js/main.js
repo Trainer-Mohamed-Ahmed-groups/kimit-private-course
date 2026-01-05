ab4cf1627821fb161e54e91278e7b894a54d87e7
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2026-01-05 22:14:37 +0200

    To array and practice

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log("Hello JS")
+
+
+// var x;
+
+// x = 10;
+
+var x = 10; // Number
+
+console.log(x)
+console.log(typeof x)
+
+
+/*                        Data types             */
+// Primitive datatype
+var username = "Yahia"; // String
+var isSold = true // Boolean
+var course; // undefined
+var test = document.getElementById('test') // null
+
+console.log(username)
+console.log(typeof username)
+console.log(course)
+console.log(test)
+
+
+// Non primitive datatype
+
+var employees = ["Ali", "Khaled", "Mai", "Nour"];
+console.log(employees)
+console.log(employees[0])
+console.log(employees[employees.length - 1])
+console.log(employees.at(-1))
+console.log(typeof employees)
