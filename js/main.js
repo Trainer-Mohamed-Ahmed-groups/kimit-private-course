81fc112bc97a299a09c12d3ad82ec142cb90c2df
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2026-01-08 20:57:55 +0200

    To strict mode

--- js/main.js
+++ js/main.js
@@ -1,34 +1,162 @@
-console.log("Hello JS")
+"use strict";
+// Object
+var car = {
+    model: 2025,
+    name: "Mercedes C300",
+    country: "Germany",
+    isSold: true,
+    owner: undefined,
+    features: ["Turbo", "10 airbags", "ABS"]
+    // Key : value
+}
+
+console.log(car)
+console.log(typeof car)
+// Bracket notation
+console.log(car["country"])
+console.log(car["features"][2])
+// Dot notation
+console.log(car.name)
+console.log(car.features[2])
 
 
-// var x;
-
-// x = 10;
-
-var x = 10; // Number
-
-console.log(x)
-console.log(typeof x)
+// VOID function
+function firstFn() {
+    console.log("Hello function")
+}
+console.log(typeof firstFn)
 
 
-/*                        Data types             */
-// Primitive datatype
-var username = "Yahia"; // String
-var isSold = true // Boolean
-var course; // undefined
-var test = document.getElementById('test') // null
+firstFn()
+firstFn()
 
-console.log(username)
-console.log(typeof username)
-console.log(course)
-console.log(test)
+// Returning value function
+function calc() {
+    return 5 + 5
+}
+
+console.log(calc())
 
 
-// Non primitive datatype
+// function calc2(x = 0, y = 0) {
+//     return x + y
+// }
 
-var employees = ["Ali", "Khaled", "Mai", "Nour"];
-console.log(employees)
-console.log(employees[0])
-console.log(employees[employees.length - 1])
-console.log(employees.at(-1))
-console.log(typeof employees)
+// var calc2 = function (x = 0, y = 0) {
+//     return x + y
+// }
+
+var calc2 = (x = 0, y = 0) => x + y
+
+console.log(calc2(10, 5))
+console.log(calc2(108, 513))
+console.log(calc2(108))
+
+console.log(typeof NaN)
+
+console.log("****************************")
+/*         Control flow                 */
+var myNum = 0
+if (myNum > 0) {
+    console.log("Greater")
+} else if (myNum < 0) {
+    console.log("Lower")
+}
+else {
+    console.log("Zero")
+}
+
+
+var y = 10;
+
+if (y !== '10') {
+    console.log("YES")
+} else {
+    console.log("NO")
+}
+
+// Unary arithmetic operators ++ --
+var t = 10;
+console.log(++t)
+console.log(t--)
+console.log(t--)
+console.log(t)
+
+// Binary arithmetic operators + - * / %
+
+console.log(12 % 3)
+console.log(8 % 2)
+
+// Relational operators > < >= <= == === != !==
+
+// Assignment operators = += -= *= /= %=
+var e = 50;
+console.log(e)
+e += 10;
+console.log(e)
+
+
+// Logical operators ! && ||
+
+if (5 > 4 || 5 > 40) {
+    console.log("YES")
+} else {
+    console.log("NO")
+}
+
+console.log("first" && 50)
+console.log("first" && 0 && "OK")
+
+
+var p = "Mahmoud"
+
+switch (p) {
+    case "Mohamed":
+    case "Mahmoud":
+        console.log("first")
+        break;
+    case "Yahia": console.log("Second")
+        break;
+    default:
+        console.log("Other")
+}
+
+///////////////////////////////
+
+try {
+    // console.log(first);
+} catch (error) {
+    console.error(error.message)
+} finally {
+    console.log("Come")
+}
+
+
+
+console.log("Hello");
+
+
+function getRectArea(width, height) {
+    if (isNaN(width) || isNaN(height)) {
+        throw "Parameters is not valid"
+    }
+    else {
+        return width * height
+    }
+}
+
+console.log(getRectArea(8, 5))
+
+try {
+    // console.log(getRectArea(8, "s"))
+    console.log(getRectArea(8, 2))
+} catch (error) {
+    console.error(error)
+}
+
+console.log("first")
+console.log(1 / 0)
+
+
+q = 90;
+console.log(q)
